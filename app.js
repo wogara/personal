a73b4f295f374e51269612fb638c145d171ce566
wogara <weogara@gmail.com>
2020-08-25 14:17:43 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app     = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("app listening on port 3000");
+});
