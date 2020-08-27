53181873cc75c274c55e27ed92291e1625e907ba
wogara <weogara@gmail.com>
2020-08-27 14:57:10 -0400

    final product

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get("/patatap",function(req,res){
     res.render("circles");
 });
 
+app.get("/resume",function(req,res){
+    res.render("resume");
+});
+
+app.get("/colorgame",function(req,res){
+    res.render("colorGame");
+});
+
 app.listen(process.env.PORT || 3000,function(){
     console.log("app listening on port 3000");
 });
